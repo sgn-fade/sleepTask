c6d66276a07d4364c9541c7276df715c951976b3
fade <dimabreusgo@gmail.com>
2024-04-12 16:31:37 +0300

    Player rotation moved to another function

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
     private Animator m_animator;
     private SpriteRenderer m_sprite;
-    [SerializeField] private float m_actionCooldown = 0.3f;
+    [SerializeField] private float actionCooldown = 0.3f;
+    private bool m_isAction = false;
     private float m_timeToAction;
     private static readonly int Attack1 = Animator.StringToHash("Attack1");
     private static readonly int Block = Animator.StringToHash("Block");
@@ -22,29 +24,36 @@ public class Player : MonoBehaviour
     private void Update()
     {
         m_timeToAction -= Time.deltaTime;
-        float inputX = Input.GetAxis("Horizontal");
+        
+        TryRotate();
+        TryAttack();
+    }
 
+    private void TryRotate()
+    {
+        if (m_isAction) return;
+        float inputX = Input.GetAxis("Horizontal");
         m_sprite.flipX = inputX switch
         {
             < 0 => true,
             > 0 => false,
             _ => m_sprite.flipX
         };
-        TryAttack();
     }
-
     private void TryAttack()
     {
         if (!(m_timeToAction <= 0)) return;
         if (Input.GetMouseButtonDown(0))
         {
+            m_isAction = true;
             m_animator.SetTrigger(Attack1);
-            m_timeToAction = m_actionCooldown;
+            m_timeToAction = actionCooldown;
         }
         else if (Input.GetMouseButtonDown(1))
         {
+            m_isAction = true;
             m_animator.SetTrigger(Block);
-            m_timeToAction = m_actionCooldown;
+            m_timeToAction = actionCooldown;
         }
 
     }
